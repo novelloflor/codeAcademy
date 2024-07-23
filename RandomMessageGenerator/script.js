7881b4ffa3d10a2c74c3de6e3c7c7fc0ac2612cb
Florencia Novello <novelloflor@gmail.com>
2024-07-23 09:56:31 +0200

    .

--- RandomMessageGenerator/script.js
+++ RandomMessageGenerator/script.js
@@ -1,3 +1,4 @@
+//Function that contains the object to be created
 const phraseBuilder = (verb, adj) => {
     let obj = {
         primePhrase: 'This week is going to feel ',
@@ -5,12 +6,14 @@ const phraseBuilder = (verb, adj) => {
         verb: verb,
         adj: adj,
         generate(){
-            phraseGenerator()
+            var phrase = `${obj.primePhrase}${adj}!. ${obj.secondaryPhrase}${verb}`
+            return phrase;
         }
     }
+    return obj
 }
 
-//returns verbs. Takes the min number of the array and the max length and randomly gets a number to pick any verb
+//returns verb and adjective. Takes the min number of the array and the max length and randomly gets a number to pick any verb/adjetive
 const returnVerb = (min, max) => {
     var words = ['loving', 'enjoying', 'eating', 'living'];
     return words[Math.floor(Math.random() * (max - min + 1)) + min];
@@ -22,13 +25,12 @@ const returnAdj = (min, max) => {
 }
 
 
-returnAdj(0, 3)
-returnVerb(0, 3)
+//Generates the random phrase each time the program runs
 
 const phraseGenerator = () => {
-
+    const phrasing = phraseBuilder(returnVerb(0, 3), returnAdj(0, 3));
+    console.log(phrasing.generate());
 }
+phraseGenerator();
 
-/*
-primePhrase: ['sunset', 'flowers', 'sky', 'ground']
-*/
+
